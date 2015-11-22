ebb20a1bf06dae212ec050581d956dc1015800c6
burghajo <burghajo@hs-albsig.de>
2015-11-22 13:23:48 +0100

    Add Maps Activity and a Swipe Detector

--- app/src/main/java/test/gpstest/MainActivity.java
+++ app/src/main/java/test/gpstest/MainActivity.java
@@ -1,15 +1,23 @@
 package test.gpstest;
 
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private float x1, x2;
+    private DisplayMetrics metrics;
+    private int MIN_DISTANCE;
+
 
     private TextView coordinates;
     private Button trackButton;
@@ -19,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        this.MIN_DISTANCE = metrics.widthPixels / 3;
 
         // assigning the button and textview
         this.coordinates = (TextView) findViewById(R.id.coordinates);
@@ -36,12 +48,30 @@ public class MainActivity extends AppCompatActivity {
     // Die Funktion, kontinuierlich GPS daten zu laden muss in einem AsyncTask geschehen.
     public void controlTrack(View v) {
 
-        if(!gps.canGetLocation()){
+        if (!gps.canGetLocation()) {
             Toast.makeText(this, "No GPS or Network connection...", Toast.LENGTH_SHORT).show();
         }
         gps.getLocation();
-        coordinates.setText("Breitengrad: " + gps.getLatitude()+"°\n" + "Längengrad: " + gps.getLongitude()+"°");
+        coordinates.setText("Breitengrad: " + gps.getLatitude() + "°\n" + "Längengrad: " + gps.getLongitude() + "°");
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x1 - x2;
+                if (deltaX > MIN_DISTANCE) {
+                    Toast.makeText(this, "right 2 left swipe with " + MIN_DISTANCE, Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
 
 }
